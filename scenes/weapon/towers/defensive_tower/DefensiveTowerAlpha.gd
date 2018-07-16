77abd88ed858bd24764f1e1dc56372320662296c
jackflower <jackflower@poczta.onet.pl>
2018-07-16 10:10:50 +0200

    Adding the DefensiveTowerBulletChamberAlpha scene
    Update scene DefensiveTowerAlpha. Adding script.

---
+++ scenes/weapon/towers/defensive_tower/DefensiveTowerAlpha.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# Scinfor - jackflower
+# July 16, 2018
+#
+# The scene represents an automatic defensive tower
+
+
+# For the scene to work properly, specify the object
+# that will be the target by calling
+# the  setTarget (Target)  function.
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
